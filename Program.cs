5b9de839d589e9f37f3e724ad7d6b0c791b2d093
Cengiz Ergün <cengizergun.000@gmail.com>
2018-11-21 07:53:02 +0300

    Add files via upload
    Exercise: Design a Stack
    A Stack is a data structure for storing a list of elements in a LIFO (last in, first out) fashion.
    Design a class called Stack with three methods.
    void Push(object obj)
    object Pop()
    void Clear()
    The Push() method stores the given object on top of the stack. We use the “object” type here so
    we can store any objects inside the stack. Remember the “object” class is the base of all classes
    in the .NET Framework. So any types can be automatically upcast to the object. Make sure to
    take into account the scenario that null is passed to this object. We should not store null
    references in the stack. So if null is passed to this method, you should throw an
    InvalidOperationException. Remember, when coding every method, you should think of all
    possibilities and make sure the method behaves properly in all these edge cases. That’s what
    distinguishes you from an “average” programmer.
    The Pop() method removes the object on top of the stack and returns it. Make sure to take into
    account the scenario that we call the Pop() method on an empty stack. In this case, this method
    should throw an InvalidOperationException. Remember, your classes should always be in a valid
    state and used properly. When they are misused, they should throw exceptions. Again, thinking
    of all these edge cases, separates you from an average programmer. The code written this way
    will be more robust and with less bugs.
    The Clear() method removes all objects from the stack.
    We should be able to use this stack class as follows:
    var stack = new Stack();
    stack.Push(1);
    stack.Push(2);
    stack.Push(3);
    Console.WriteLine(stack.Pop());
    Console.WriteLine(stack.Pop());
    Console.WriteLine(stack.Pop());
    The output of this program will be
    3
    2
    1
    Note: The downside of using the object class here is that if we store value types (eg int, char,
    bool, DateTime) in our Stack, boxing and unboxing occurs, which comes with a small
    performance penalty. In my C# Advanced course, I’ll teach you how to resolve this by using
    generics, but for now don’t worry about it.
    2
    Real-world use case: Stacks are very popular in real-world applications. Think of your browser.
    As you navigate the web, the address of each page you visit is stored in a stack. As you click the
    Back button, the most recent address is popped. This is because of the LIFO behaviour of
    stacks.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yeni = new Stack();
+
+            yeni.Push(1);
+            yeni.Push(2);
+            yeni.Push(3);
+            yeni.Push("a");
+
+            yeni.Clear();
+
+            Console.WriteLine(yeni.pop());
+            Console.WriteLine(yeni.pop());
+            Console.WriteLine(yeni.pop());
+
+            yeni.Clear();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackApp
+{
+    class Stack
+    {
+        
+        private List<object> lists = new List<object>();
+        private List<object> otherlists = new List<object>();
+
+        public void Push(object obje)
+        {
+            if(obje==null)
+            {
+                throw new System.InvalidOperationException();
+            }
+            else
+            {
+                lists.Add(obje);
+                otherlists = lists.ToList();
+                otherlists.Reverse();
+            }
+        }
+        public object pop()
+        {
+            if(otherlists==null)
+            {
+                throw new System.InvalidOperationException();
+            }
+            else
+            {
+                object x = otherlists[0];
+                otherlists.RemoveAt(0);
+                return x;
+            }
+        }
+        public void Clear()
+        {
+            otherlists.Clear();
+        }
+
+
+    }
+}
